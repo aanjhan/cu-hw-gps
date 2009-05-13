2c6d90f80088b0b453d97056fc04ac8f317ab806
adam <adam@c395ad66-ce3e-11de-b393-33af5e5426e5>
2009-05-13 23:36:13 +0000

    Moved twos-complement function to separate file. Added Linux/Mac serial transfer via C transfer utility.

--- gps/matlab/test_gen/GPSPack.m
+++ gps/matlab/test_gen/GPSPack.m
@@ -16,9 +16,3 @@ function packedData=GPSPack(data)
         end
     end
 end
-
-function val=TwosToVal(data)
-    data=mod(data+4,8);
-    twos=[7 6 5 0 1 2 3];
-    val=twos(data);
-end
--- gps/matlab/test_gen/TrackTest.m
+++ gps/matlab/test_gen/TrackTest.m
@@ -6,24 +6,34 @@ function [signal,packedSignal]=TrackTest(PRN,send)
     signal=digitize_ca_prompt(caCode,length);
     signal=signal*2-1;
     
+    signal=signal(0+(1:1000));
+    
     packedSignal=GPSPack(signal);
+    signal=TwosToVal(signal)';
+    
+    frame=[hex2dec('FE');
+        hex2dec('ED');
+        2;
+        floor(size(signal,1)/2^8);
+        mod(size(signal,1),2^8);
+        signal];
+%     frame=[hex2dec('FE');
+%         hex2dec('ED');
+%         1;
+%         floor(size(packedSignal,1)/2^8);
+%         mod(size(packedSignal,1),2^8);
+%         packedSignal];
+
+    filename=sprintf('prn_%d.dat',PRN);
+    file=fopen(filename,'wb');
+    if(file<0)
+        disp(sprintf('Unable to open file prn_%d.dat.',PRN));
+        return;
+    end
+    fwrite(file,frame);
+    fclose(file);
     
     if(nargin>1 && send)
-        frame=[hex2dec('FE');
-               hex2dec('ED');
-               1;
-               floor(size(packedSignal,1)/2^8);
-               mod(size(packedSignal,1),2^8);
-               packedSignal];
-        
-        file=fopen(sprintf('prn_%d.dat',PRN),'wb');
-        if(file<0)
-            disp(sprintf('Unable to open file prn_%d.dat.',PRN));
-            return;
-        end
-        fwrite(file,frame);
-        fclose(file);
-        
         if(strcmp(computer,'PCWIN'))
             bar=waitbar(0,sprintf('Transfer progress: %dB remaining.',size(frame,1)));
             s=serial('COM4','BaudRate',115200);
@@ -34,6 +44,9 @@ function [signal,packedSignal]=TrackTest(PRN,send)
             end
             close(bar);
             fclose(s);
+        else
+            [status results]=system(sprintf('./transfer /dev/tty.usbserial %s',filename));
+            disp(results);
         end
     end
 end

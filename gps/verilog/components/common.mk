2952f85cccb8cc7135867d59e956d5a997ced07f
adam <adam@c395ad66-ce3e-11de-b393-33af5e5426e5>
2009-06-02 15:59:22 +0000

    Added definition conflict check as first step in make flow.

--- gps/verilog/components/common.mk
+++ gps/verilog/components/common.mk
@@ -7,8 +7,17 @@ DEFPARSER?=defparser
 
 SOURCES+=$(COMPONENTS_DIR)/global.csv
 
-all:
+.phony: all
+all: conflict_check headers
+
+.phony: conflict_check
+conflict_check: $(SOURCES)
+	@echo Checking for macro conflicts...
+	@perl -e '$$err=`$(DEFPARSER) $(SOURCES) 2>&1 1>/dev/null`; print $$err; exit(1) if $$err ne "";'
+
+.phony: headers
+headers: $(SOURCES)
 	@for s in $(SOURCES); do \
-	echo Parsing $${s}; \
+	echo Parsing $${s}...; \
 	$(DEFPARSER) -o `perl -e 'print "$$1.vh" if "'$${s}.'"=~/(.*)\.[^.]+/;'` $${s}; \
 	done

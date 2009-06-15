032da07ace890e823fa79badf082df4318c5b6a1
adam <adam@c395ad66-ce3e-11de-b393-33af5e5426e5>
2009-06-15 01:49:37 +0000

    Updated test generator GPS data load function to generics. Added data load feature to test generator.

--- gps/matlab/test_gen/test_gen.m
+++ gps/matlab/test_gen/test_gen.m
@@ -5,20 +5,27 @@ function [signal,carrier,code,t]=test_gen(PRN,pack,filename,send)
     %Signal length in C/A codes.
     acc_length=1;
     range=1:acc_length*FSAMP_MSEC;
-%     range=1:4000;
-    
-    caCode=cacodegn(PRN);
-    code=digitize_ca_prompt(caCode,acc_length);
-    code=code*2-1;
-    code=code(range);
-    
-    doppler=0;
-    f_carrier=FC+doppler;
+    %     range=1:4000;
     t=0:1/FS:acc_length*T-1/FS;
     t=t(range);
-    carrier=round(cos(2*pi*f_carrier*t)'*3);
     
-    signal=code.*carrier;
+    if(isnumeric(PRN))
+        caCode=cacodegn(PRN);
+        code=digitize_ca_prompt(caCode,acc_length);
+        code=code*2-1;
+        code=code(range);
+
+        doppler=200;
+        f_carrier=FC+doppler;
+        carrier=round(cos(2*pi*f_carrier*t)'*3);
+
+        signal=code.*carrier;
+    else
+        code=0;
+        carrier=0;
+        signal=load_gps_data(PRN,0,1);
+        signal=signal(range);
+    end
     
     packedSignal=gps_pack(signal);
     ones_signal=twos_to_ones(signal,3);
@@ -41,10 +48,13 @@ function [signal,carrier,code,t]=test_gen(PRN,pack,filename,send)
 
     if(nargin<3 || isempty(filename))
         return;
-    else
+    elseif(~isnumeric(PRN))
         filename=sprintf('_%s',filename);
     end
-    filename=sprintf('prn%d%s',PRN,filename);
+    
+    if(isnumeric(PRN))
+        filename=sprintf('prn%d%s',PRN,filename);
+    end
     
     file=fopen(sprintf('%s.dat',filename),'wb');
     if(file<0)

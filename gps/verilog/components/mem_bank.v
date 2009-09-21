e5aac0e36f35edaf935fe0f923fe22d69fdc41df
tommy <tommy@c395ad66-ce3e-11de-b393-33af5e5426e5>
2009-09-21 01:50:45 +0000

    mem_bank.csv - Added extra parameters for parameterization of mem_bank.v.
    mem_bank.v - Finished parameterization and corrected address update conditions.  
    
    mem_bank.v tested with 1ms simulated data and shows proper functionality.

--- gps/verilog/components/mem_bank.v
+++ gps/verilog/components/mem_bank.v
@@ -21,8 +21,7 @@ module mem_bank (
 	reg [`ADDRESS_RANGE] addr;
 	reg [`ADDRESS_RANGE] start_addr;
 	
-	//FIXME add reference to acq_time in mem_bank.csv to do this range
-	reg [15:0] sample_counter; //wide enough to store # samples in acq time
+	reg [`SAMPLE_COUNT_RANGE] sample_counter; //wide enough to store # samples in acq time
 	
 	reg [`OFFSET_RANGE] offset;
 	
@@ -46,14 +45,14 @@ module mem_bank (
              .q(m4k_out)
 	);
 	
-	//FIXME parameterize by adding reference to SAMPLE_WIDTH in mem_bank.csv
-	assign data_out = buffer[23:21];
+//	assign data_out = buffer[23:21];
+    assign data_out = buffer[`DATA_OUT_RANGE];
 	assign m4k_in = buffer;
 	
 	delay write_enable_flag_delay (
       .clk(clk),
       .reset(reset),
-      .in(data_available && (offset == `OFFSET_LENGTH'd7)), //FIXME The value 7 needs to be parameterized
+      .in(data_available && (offset == `BUFFER_MAX_OFFSET)),
       .out(m4k_wren));
 	
 	always @ (posedge clk) begin
@@ -64,17 +63,17 @@ module mem_bank (
          buffer <= `WORD_LENGTH'h0;
          addr <= addr;
          start_addr <= start_addr;
-         sample_counter <= 16'd0; //FIXME parameterize
+         sample_counter <= `SAMPLE_COUNT_WIDTH'd0;
   
       end
       
       else if (mode_change) begin
          
-         offset <= `OFFSET_LENGTH'd5;  //FIXME parameterize 5
+         offset <= `BUFFER_MAX_OFFSET_M2;
          buffer <= buffer;
          addr <= (mode == `MODE_PLAYBACK) ? start_addr : `ADDRESS_LENGTH'h0;
          start_addr <= (mode == `MODE_PLAYBACK) ? start_addr : `ADDRESS_LENGTH'h0;
-         sample_counter <= (mode == `MODE_PLAYBACK) ? 16'd50397 : 16'd0; //FIXME: parameterize
+         sample_counter <= (mode == `MODE_PLAYBACK) ? `SAMPLE_COUNT_WIDTH'd`SAMPLES_PER_ACQ_M3 : `SAMPLE_COUNT_WIDTH'd0;
          
       end
       
@@ -82,27 +81,31 @@ module mem_bank (
       
          if (mode == `MODE_PLAYBACK) begin
             
-            buffer <= (offset == `OFFSET_LENGTH'd7) ? m4k_out : {buffer[20:0],`OFFSET_LENGTH'h0}; //FIXME: PARAMETERIZE
+            buffer <= (offset == `BUFFER_MAX_OFFSET || sample_counter == `SAMPLE_COUNT_WIDTH'd`SAMPLES_PER_ACQ_M1) ? m4k_out : 
+                           {buffer[`DATA_IN_RANGE],`OFFSET_LENGTH'h0};
             offset <= offset + `OFFSET_LENGTH'd1;
-            addr <= (offset != `OFFSET_LENGTH'd1) ? addr :
-                    (sample_counter == 16'd50399) ? start_addr : //FIXME
-                    (addr == `ADDRESS_LENGTH'd`NUM_WORDS - 1) ? `ADDRESS_LENGTH'd0 :
+            addr <= (sample_counter == `SAMPLE_COUNT_WIDTH'd`SAMPLES_PER_ACQ_M4) ? start_addr :
+                    (offset != `OFFSET_LENGTH'd1) ? addr :
+                    (addr == `ADDRESS_LENGTH'd`NUM_WORDS_M1) ? `ADDRESS_LENGTH'd0 :
                     addr + `ADDRESS_LENGTH'd1 ;
                     
             start_addr <= start_addr;
-            sample_counter <= (sample_counter == 16'd50399) ? 16'd0 : sample_counter + 16'd1; //FIXME
+            sample_counter <= (sample_counter == `SAMPLE_COUNT_WIDTH'd`SAMPLES_PER_ACQ_M1) ? `SAMPLE_COUNT_WIDTH'h0 : sample_counter + `SAMPLE_COUNT_WIDTH'h1;
          end
          
          else begin // mode == `MODE_WRITING
 
-            buffer <= data_available ? {buffer[20:0],data_in} : buffer;  //FIXME
+            buffer <= data_available ? {buffer[`DATA_IN_RANGE],data_in} : buffer;
             offset <= data_available ? offset + `OFFSET_LENGTH'd1 : offset;
             addr <= !m4k_wren ? addr :
-                    (addr == `ADDRESS_LENGTH'd`NUM_WORDS - 1) ? `ADDRESS_LENGTH'd0 :
+                    (addr == `ADDRESS_LENGTH'd`NUM_WORDS_M1) ? `ADDRESS_LENGTH'd0 :
                     addr + `ADDRESS_LENGTH'd1;
 
-            start_addr <= data_available && (sample_counter == 16'd50400) ? start_addr + `ADDRESS_LENGTH'd1 : start_addr; //FIXME
-            sample_counter <= !data_available || (sample_counter == 16'd50400) ? sample_counter : sample_counter + 16'd1;
+            start_addr <= !m4k_wren || (sample_counter != `SAMPLE_COUNT_WIDTH'd`SAMPLES_PER_ACQ) ? start_addr :
+                           start_addr == `ADDRESS_LENGTH'd`NUM_WORDS_M1 ? `ADDRESS_LENGTH'd0 : 
+                           start_addr + `ADDRESS_LENGTH'd1;
+                           
+            sample_counter <= !data_available || (sample_counter == `SAMPLE_COUNT_WIDTH'd`SAMPLES_PER_ACQ) ? sample_counter : sample_counter + `SAMPLE_COUNT_WIDTH'h1;
 
          end
       end
@@ -115,10 +118,9 @@ module mem_bank (
       .in(mode),
       .out(mode_change));
 
-   //FIXME
-   assign ready = (mode == `MODE_WRITING) && (sample_counter == 16'd50400) || (mode == `MODE_PLAYBACK);
-   assign frame_start = (mode == `MODE_PLAYBACK) && (sample_counter == 16'd0);
-   assign frame_end = (mode == `MODE_PLAYBACK) && (sample_counter == 16'd50399);
+   assign ready = (mode == `MODE_WRITING) && (sample_counter == `SAMPLE_COUNT_WIDTH'd`SAMPLES_PER_ACQ) || (mode == `MODE_PLAYBACK);
+   assign frame_start = (mode == `MODE_PLAYBACK) && (sample_counter == `SAMPLE_COUNT_WIDTH'd0);
+   assign frame_end = (mode == `MODE_PLAYBACK) && (sample_counter == `SAMPLE_COUNT_WIDTH'd`SAMPLES_PER_ACQ_M1);
    assign sample_valid = (mode == `MODE_PLAYBACK) && (!mode_change);
 
 endmodule

56479f9f1877d9c3d45408e641f834a384b0bd15
adam <adam@c395ad66-ce3e-11de-b393-33af5e5426e5>
2009-10-27 21:35:55 +0000

    rtdf_packet_processor.csv:    Added additional FIFO buffering room. rt_data_feed.v:    Switched halt input to control sampler.    Added note about watchdog and packet processor halt condition.

--- gps/verilog/rt_data_feed/rt_data_feed.v
+++ gps/verilog/rt_data_feed/rt_data_feed.v
@@ -30,6 +30,11 @@ module rt_data_feed(
     input              halt,
     input              halt_packet);
 
+   //FIXME Add a watchdog to reset packet processor/RX FIFO
+   //FIXME if stuck in data state too long. This might happen
+   //FIXME if the processor halts the RX FIFO and that then
+   //FIXME halts the DM9000A, resulting in data loss.
+
    /////////////////////////
    // Ethernet Controller
    /////////////////////////
@@ -52,7 +57,7 @@ module rt_data_feed(
                               .rx_fifo_rd_req(rx_fifo_rd_req),
                               .rx_fifo_rd_data(rx_fifo_rd_data),
                               .rx_fifo_empty(rx_fifo_empty),
-                              .halt(halt),
+                              .halt(1'b0),
                               .link_status(link_status));
 
    ////////////////////
@@ -90,46 +95,52 @@ module rt_data_feed(
 
    `PRESERVE reg [17:0] sample_buffer;
    `PRESERVE reg [1:0]  sample_extra;
-   always @(posedge clk_sample or posedge reset) begin
-      //Flag when samples are valid.
-      sample_valid <= reset ? 1'b0 : sample_count>3'd0;
-        
-      //Words contain 5 whole 3b samples, and one extra
-      //bit. Increment the sample count by 6 if there
-      //are already 2 extra bits available, and by
-      //5 otherwise.
-      sample_count <= reset ? 3'd0 :
-                      sample_count>3'd1 ? sample_count-3'd1 :
-                      packet_empty ? (sample_count==3'd1 :
-                                      3'd0 :
-                                      sample_count) :
-                      sample_extra==2'd2 ? 3'd6 :
-                      3'd5;
+   always @(posedge clk_sample) begin
+      if(reset) begin
+         sample_valid <= 1'b0;
+         sample_count <= 3'd0;
+         sample_extra <= 2'd0;
+         sample_buffer <= 18'h0;
+         sample_data <= 3'd0;
+      end
+      else if(!halt) begin
+         //Flag when samples are valid.
+         sample_valid <= sample_count>3'd0;
+         
+         //Words contain 5 whole 3b samples, and one extra
+         //bit. Increment the sample count by 6 if there
+         //are already 2 extra bits available, and by
+         //5 otherwise.
+         sample_count <= sample_count>3'd1 ? sample_count-3'd1 :
+                         packet_empty ? (sample_count==3'd1 :
+                                         3'd0 :
+                                         sample_count) :
+                         sample_extra==2'd2 ? 3'd6 :
+                         3'd5;
       
-      //Each word has one extra bit. Increment count
-      //by one until a whole sample (3b) is built.
-      sample_extra <= reset ? 2'd0 :
-                      !packet_read ? sample_extra :
-                      sample_extra==2'd2 ? 2'd0 :
-                      sample_extra+2'd1;
-
-      //Shift the buffer left by one sample each cycle,
-      //and append a data word when appropriate.
-      sample_buffer <= reset ? 18'h0 :
-                       sample_count>3'd1 ? {2'h0,sample_buffer[16:3]} :
-                       packet_empty ? (sample_count==3'd1 ?
-                                       {2'h0,sample_buffer[16:3]} :
-                                       sample_buffer) :
-                       sample_extra==2'd0 ? {2'h0,packet_data} :
-                       sample_count==3'd1 ? (sample_extra==2'd1 ?
-                                             {1'h0,packet_data,sample_buffer[3]} :
-                                             {packet_data,sample_buffer[4:3]}) :
-                       (sample_extra==2'd1 ?
-                        {1'h0,packet_data,sample_buffer[0]} :
-                        {packet_data,sample_buffer[1:0]});
-
-      //Sample data is the lowest 3 bits in the buffer.
-      sample_data <= reset ? 3'd0 : sample_buffer[2:0];
+         //Each word has one extra bit. Increment count
+         //by one until a whole sample (3b) is built.
+         sample_extra <= !packet_read ? sample_extra :
+                         sample_extra==2'd2 ? 2'd0 :
+                         sample_extra+2'd1;
+         
+         //Shift the buffer left by one sample each cycle,
+         //and append a data word when appropriate.
+         sample_buffer <= sample_count>3'd1 ? {2'h0,sample_buffer[16:3]} :
+                          packet_empty ? (sample_count==3'd1 ?
+                                          {2'h0,sample_buffer[16:3]} :
+                                          sample_buffer) :
+                          sample_extra==2'd0 ? {2'h0,packet_data} :
+                          sample_count==3'd1 ? (sample_extra==2'd1 ?
+                                                {1'h0,packet_data,sample_buffer[3]} :
+                                                {packet_data,sample_buffer[4:3]}) :
+                          (sample_extra==2'd1 ?
+                           {1'h0,packet_data,sample_buffer[0]} :
+                           {packet_data,sample_buffer[1:0]});
+         
+         //Sample data is the lowest 3 bits in the buffer.
+         sample_data <= sample_buffer[2:0];
+      end
    end // always @ (negedge clk_sample)
 
    //FIXME Remove these.

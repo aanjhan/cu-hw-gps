4518e59870a9b06cbde62e1d7a42cbe80bcc86ae
adam <adam@c395ad66-ce3e-11de-b393-33af5e5426e5>
2009-11-27 03:41:50 +0000

    rt_data_feed.v, sampler.v:    Moved data sampler to separate module for testing purposes.    Corrected sampler buffer top bit truncation error.

--- gps/verilog/rt_data_feed/rt_data_feed.v
+++ gps/verilog/rt_data_feed/rt_data_feed.v
@@ -33,19 +33,16 @@ module rt_data_feed(
     inout wire [15:0]  enet_data,
     //Sample interface.
     input              clk_sample,
-    output reg         sample_valid,
-    output reg [2:0]   sample_data,
+    output wire        sample_valid,
+    output wire [2:0]  sample_data,
     //Debug signals.
     output wire        link_status,
-    output wire        have_data,
     output wire [8:0]  words_available,
     output wire [8:0]  packet_count,
     output wire [8:0]  good_packet_count,
     output wire [8:0]  missed_count,
-    output wire [15:0] data_out,
     //Debug
-    output reg [31:0]  total_sample_count,
-    input              halt,
+    output wire [31:0] total_sample_count,
     input              halt_packet);
 
    //FIXME Add a watchdog to reset packet processor/RX FIFO
@@ -98,72 +95,18 @@ module rt_data_feed(
                                    .packet_count(packet_count),
                                    .good_packet_count(good_packet_count),
                                    .missed_count(missed_count));
-   
-   assign have_data = !packet_empty;
-   assign data_out = packet_data;
-   
-   //When there are less than two samples in the
-   //buffer (there are at least 16b available for
-   //a FIFO read), read a word from the FIFO.
-   `PRESERVE reg [2:0]  sample_count;
-   assign packet_read = !packet_empty && sample_count<3'd2;
 
    ////////////////////
    // Sample Generator
    ////////////////////
 
-   `PRESERVE reg [17:0] sample_buffer;
-   `PRESERVE reg [1:0]  sample_extra;
-   always @(negedge clk_sample) begin
-      if(reset) begin
-         sample_valid <= 1'b0;
-         sample_count <= 3'd0;
-         sample_extra <= 2'd0;
-         sample_buffer <= 18'h0;
-         sample_data <= 3'd0;
-
-         total_sample_count <= 32'd0;
-      end
-      else if(!halt) begin
-         //Flag when samples are valid.
-         sample_valid <= sample_count>3'd0;
-
-         total_sample_count <= sample_count>3'd0 ?
-                               total_sample_count+32'd1 :
-                               total_sample_count;
-         
-         //Words contain 5 whole 3b samples, and one extra
-         //bit. Increment the sample count by 6 if there
-         //are already 2 extra bits available, and by
-         //5 otherwise.
-         sample_count <= sample_count>3'd1 ? sample_count-3'd1 :
-                         packet_empty ? 3'd0 :
-                         sample_extra==2'd2 ? 3'd6 :
-                         3'd5;
-      
-         //Each word has one extra bit. Increment count
-         //by one until a whole sample (3b) is built.
-         sample_extra <= !packet_read ? sample_extra :
-                         sample_extra==2'd2 ? 2'd0 :
-                         sample_extra+2'd1;
-         
-         //Shift the buffer left by one sample each cycle,
-         //and append a data word when appropriate.
-         sample_buffer <= sample_count>3'd1 ? {2'h0,sample_buffer[16:3]} :
-                          packet_empty ? (sample_count==3'd1 ?
-                                          {2'h0,sample_buffer[16:3]} :
-                                          sample_buffer) :
-                          sample_extra==2'd0 ? {2'h0,packet_data} :
-                          sample_count==3'd1 ? (sample_extra==2'd1 ?
-                                                {1'h0,packet_data,sample_buffer[3]} :
-                                                {packet_data,sample_buffer[4:3]}) :
-                          (sample_extra==2'd1 ?
-                           {1'h0,packet_data,sample_buffer[0]} :
-                           {packet_data,sample_buffer[1:0]});
-         
-         //Sample data is the lowest 3 bits in the buffer.
-         sample_data <= sample_buffer[2:0];
-      end
-   end // always @ (negedge clk_sample)
+   sampler data_sampler(.clk_sample(clk_sample),
+                        .reset(reset),
+                        .packet_empty(packet_empty),
+                        .packet_data(packet_data),
+                        .packet_read(packet_read),
+                        .sample_valid(sample_valid),
+                        .sample_data(sample_data),
+                        .total_sample_count(total_sample_count));
    
 endmodule
